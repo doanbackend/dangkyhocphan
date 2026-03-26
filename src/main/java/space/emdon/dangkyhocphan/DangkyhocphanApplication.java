12c080d0974756c638d0e18c93e2de372f239014
doanbackend <doanbackend@gmail.com>
2026-03-26 11:25:49 +0800

    init project

---
+++ src/main/java/space/emdon/dangkyhocphan/DangkyhocphanApplication.java
@@ -0,0 +1,13 @@
+package space.emdon.dangkyhocphan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangkyhocphanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangkyhocphanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/emdon/dangkyhocphan/DangkyhocphanApplicationTests.java
@@ -0,0 +1,13 @@
+package space.emdon.dangkyhocphan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangkyhocphanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
